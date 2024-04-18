598ecbb649e44f88ccefe957dff614bfd87a607f
Asma Elhadi <asmaa.alhadi100@gmail.com>
2024-04-18 02:13:39 +0200

    2

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,9 @@ from datetime import datetime
 
 
 class BaseModel:
+    """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
+        """Instatntiates a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
@@ -13,16 +15,12 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            # Ensure 'updated_at' is provided in kwargs
-            if 'updated_at' not in kwargs:
-                kwargs['updated_at'] = datetime.now()
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
-        
 
     def __str__(self):
         """Returns a string representation of the instance"""
