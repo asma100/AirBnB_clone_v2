a170a1398ace2d255d44447c1a42ace3139bff33
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-05-20 04:55:28 -0700

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,52 +10,35 @@ env.user = 'ubuntu'
 env.key_filename = '/root/.ssh/school'
 
 
-@task
-def do_pack():
-    """ Fabric script that generates a .tgz archive from the contents of the
-    web_static folder """
-    local("sudo mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = "versions/web_static_{}.tgz".format(date)
-    result = local("sudo tar -cvzf {} web_static".format(filename))
-    if result.succeeded:
-        return filename
-    else:
-        return None
 
-
-@task
 def do_deploy(archive_path):
-    """ Distribute archive to web servers """
-    if not exists(archive_path):
-        print(f"Error: Archive file {archive_path} not found")
-        return False
+    """ distributes an archive to my web servers
+    """
+    if exists(archive_path) is False:
+        return False  # Returns False if the file at archive_path doesnt exist
+    filename = archive_path.split('/')[-1]
+    # so now filename is <web_static_2021041409349.tgz>
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    # curr = '/data/web_static/current'
+    tmp = "/tmp/" + filename
 
     try:
-        # Upload the archive to the /tmp/ directory on the web servers
         put(archive_path, "/tmp/")
-        archive_filename = os.path.basename(archive_path)
-        archive_name_without_ext = os.path.splitext(archive_filename)[0]
-        release_path = f'/data/web_static/releases/{archive_name_without_ext}'
-
-        # Uncompress the archive to the /data/web_static/releases/ directory
-        run(f'mkdir -p {release_path}')
-        run(f'tar -xzf /tmp/{archive_filename} -C {release_path}')
-        
-        # Move contents to the correct location
-        run(f'mv {release_path}/web_static/* {release_path}/')
-        run(f'rm -rf {release_path}/web_static')
-
-        # Delete the archive from the web servers
-        run(f'rm /tmp/{archive_filename}')
-
-        # Delete the symbolic link /data/web_static/current
-        run('rm -rf /data/web_static/current')
-
-        # Create a new symbolic link /data/web_static/current
-        run(f'ln -s {release_path} /data/web_static/current')
-
+        # ^ Upload the archive to the /tmp/ directory of the web server
+        run("mkdir -p {}/".format(no_tgz))
+        # Uncompress the archive to the folder /data/web_static/releases/
+        # <archive filename without extension> on the web server
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
+        # ^ Delete the archive from the web server
+        run("rm -rf /data/web_static/current")
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
+        # Create a new the symbolic link /data/web_static/current on the
+        # web server, linked to the new version of your code
+        # (/data/web_static/releases/<archive filename without extension>)
         return True
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    except:
         return False
