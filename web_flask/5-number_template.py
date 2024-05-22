d9b4b4f1a69ff19abace07bf1109fa7e82623153
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-05-22 10:39:20 -0700

    5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,6 +2,7 @@
 """A simple Flask web application with multiple routes"""
 from flask import Flask
 from markupsafe import escape
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -27,6 +28,16 @@ def show_c_text(text):
 def show_python_text(text):
     text = text.replace('_', ' ')
     return f'Python {escape(text)}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def handle_number(n):
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def handle_number_html(n):
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == "__main__":
